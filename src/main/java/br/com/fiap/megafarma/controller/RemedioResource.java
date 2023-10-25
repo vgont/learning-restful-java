c874d082505798d5c3a4f78b067bb593333f7465
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-25 11:38:59 -0300

    add update, delete and findAllById method

--- src/main/java/br/com/fiap/megafarma/controller/RemedioResource.java
+++ src/main/java/br/com/fiap/megafarma/controller/RemedioResource.java
@@ -6,9 +6,12 @@ import br.com.fiap.megafarma.model.entity.Remedio;
 import br.com.fiap.megafarma.model.repository.RemedioRepository;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -26,6 +29,16 @@ public class RemedioResource {
 		return response.build();
 	}
 	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response findAllById(@PathParam("id") Long id) {
+		Remedio resposta = RemedioRepository.findAllByIdRemedio(id);
+		ResponseBuilder response = Response.ok();
+		response.entity(resposta);
+		return response.build();
+	}
+	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response save(@Valid Remedio remedio) {
@@ -37,6 +50,28 @@ public class RemedioResource {
 		response.entity(resposta);
 		return response.build();
 	
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public Response delete(@PathParam("id") Long id) {
+		ResponseBuilder response = Response.noContent();
+		if(!RemedioRepository.delete(id)) {
+			response = response.status(404);
+		}
+		return response.build();
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response update(@Valid Remedio remedio) {
+		Remedio resposta = RemedioRepository.update(remedio);
+		ResponseBuilder response= Response.created(null);
+		if(resposta == null) {
+			response.status(404);
+		}
+		response.entity(resposta);
+		return response.build();
 	}
 }
 
--- src/main/java/br/com/fiap/megafarma/model/repository/RemedioRepository.java
+++ src/main/java/br/com/fiap/megafarma/model/repository/RemedioRepository.java
@@ -12,7 +12,7 @@ public class RemedioRepository extends Repository {
 
 	public static ArrayList<Remedio> findAllRemedio(){
 		ArrayList<Remedio> remedios = new ArrayList<Remedio>();
-		String sql = "select * from remedio";
+		String sql = "select * from tb_remedios";
 		try {
 			PreparedStatement ps = getConnection().prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
@@ -26,20 +26,42 @@ public class RemedioRepository extends Repository {
 					remedio.setDataDeValidade(rs.getDate("data_de_validade").toLocalDate());
 					remedios.add(remedio);
 				}
-			} else {
-				return null;
-			}
+				return remedios;
+			} 
 		} catch (SQLException e) {
 			System.out.println("Erro ao listar remedios: " + e.getMessage());
 		} finally {
 			closeConnection();
 		}
+		return null;
+	}
+	
+	public static Remedio findAllByIdRemedio(Long id) {
+		String sql = "select * from tb_remedios where id=?";
+		try {
+			PreparedStatement ps = getConnection().prepareStatement(sql);
+			ps.setLong(1, id);
+			ResultSet rs = ps.executeQuery();
+			if (rs != null) {
+				Remedio remedio = new Remedio();
+				remedio.setId(rs.getLong("id"));
+				remedio.setNome(rs.getString("nome"));
+				remedio.setPreco(rs.getDouble("preco"));
+				remedio.setDataDeFabricacao(rs.getDate("data_de_fabricacao").toLocalDate());
+				remedio.setDataDeValidade(rs.getDate("data_de_validade").toLocalDate());
+				return remedio;
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			closeConnection();
+		}
 		
-		return remedios;
+		return null;
 	}
 	
 	public static Remedio save(Remedio remedio) {
-		String sql = "insert into remedio(id,nome,preco,data_de_fabricacao,data_de_validade) "
+		String sql = "insert into tb_remedios(id,nome,preco,data_de_fabricacao,data_de_validade) "
 				+ "values(null,?,?,?,?)";
 		try {
 			PreparedStatement ps = getConnection().prepareStatement(sql);
@@ -56,6 +78,45 @@ public class RemedioRepository extends Repository {
 			closeConnection();
 		}
 		
+		return null;
+	}
+	
+	public static boolean delete(Long id) {
+		String sql = "delete from tb_remedios where id=?";
+		try {
+			PreparedStatement ps = getConnection().prepareStatement(sql);
+			ps.setLong(1, id);
+			if (ps.executeUpdate() > 0) {
+				return true;
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			closeConnection();
+		}
+		
+		return false;
+	}
+	
+	public static Remedio update(Remedio remedio) {
+		String sql = "update tb_remedios "
+				+ "set nome=?, preco=?, data_de_fabricacao=?, data_de_validade=? "
+				+ "where id=?";
+		try {
+			PreparedStatement ps = getConnection().prepareStatement(sql);
+			ps.setString(1, remedio.getNome());
+			ps.setDouble(2, remedio.getPreco());
+			ps.setDate(3, Date.valueOf(remedio.getDataDeFabricacao()));
+			ps.setDate(4, Date.valueOf(remedio.getDataDeValidade()));
+			ps.setLong(5, remedio.getId());
+			if(ps.executeUpdate() > 0) {
+				return remedio;
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			closeConnection();
+		}
 		return null;
 	}
 }
